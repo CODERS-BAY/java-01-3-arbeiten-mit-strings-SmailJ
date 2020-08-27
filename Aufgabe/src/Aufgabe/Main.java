18aa1b687dfe1045fc8b70ca59e478a3e9bd4b18
Smail <smail1997@yahoo.de>
2020-08-27 09:31:07 +0200

    //edit

---
+++ Aufgabe/src/Aufgabe/Main.java
@@ -0,0 +1,19 @@
+package Aufgabe;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String helloWorld=" Hello World ";
+        String string1 = new String(helloWorld);
+
+        System.out.println(helloWorld+" hat "+string1.length()+" Zeichen");
+        System.out.println(helloWorld.toUpperCase());
+        System.out.println(helloWorld.toLowerCase());
+        helloWorld = helloWorld.replace("World", "Codersbay");
+        System.out.println(helloWorld);
+        String trimHelloWorld=helloWorld.replaceAll("^\\s+", "");
+        System.out.println(trimHelloWorld);
+    }
+}
